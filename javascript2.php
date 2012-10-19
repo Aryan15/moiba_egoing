23c7c435bf4963dbd631b496b30e92ab73b55da6
egoing <egoing@gmail.com>
2012-10-19 17:11:32 +0900

    apply bootstrap

---
+++ javascript2.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<script>
+			var i = 0;
+			while (i < 3) {
+				document.write(i);
+				document.write(',');
+				i++;
+			}
+		</script>
+	</head>
+	<body>
+	</body>
+</html>
---
+++ parameter.php
@@ -0,0 +1,4 @@
+<?php
+$arr = 데이터베이스;
+print_r($arr[rand(0,2)]);
+?>
